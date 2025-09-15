2c06d3f22290c0bac5f1ee06b868d38b0aad1f96
Khadine Rambert <Khadine.rambert@gmail.com>
2025-09-15 00:36:49 -0400

    Update Dessert.cpp

--- Dessert.cpp
+++ Dessert.cpp
@@ -1,5 +1,20 @@
 //
-// Created by Khadi on 19/12/2024.
+// Created by Khadi on 20/11/2024.
 //
-
+#include <iostream>
+using namespace std;
+#include <string>
 #include "Dessert.h"
+
+// Constructor
+    Dessert::Dessert (int itemID, string name, double price, int sweetnessLevel) : Item (itemID, name, price), sweetnessLevel(sweetnessLevel) {}
+
+// Accessors
+    int Dessert::getSweetnessLevel () {
+        return sweetnessLevel;
+    };
+
+// Mutators
+    void Dessert::setSweetnessLevel (int sweetnessLevel) {
+        this->sweetnessLevel = sweetnessLevel;
+    };
