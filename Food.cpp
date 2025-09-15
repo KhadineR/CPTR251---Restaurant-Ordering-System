8cb25f4df04823238ed4651c395f7842b44992aa
Khadine Rambert <Khadine.rambert@gmail.com>
2025-09-15 00:40:01 -0400

    Update Food.cpp

--- Food.cpp
+++ Food.cpp
@@ -1,1 +1,19 @@
+//
+// Created by Khadi on 20/11/2024.
+//
+#include <iostream>
+using namespace std;
+#include "Food.h"
 
+// Constructor
+    Food::Food (int itemID, string name, double price, string portion) : Item (itemID, name, price), portion (portion) {}
+
+// Accessors
+    string Food::getPortion () {
+        return portion;
+    };
+
+// Mutators
+    void Food::setPortion (string portion) {
+        this->portion = portion;
+    };
